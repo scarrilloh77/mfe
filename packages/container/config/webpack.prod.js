73fb1ddeb02b41f681a970023faee0f182c46388
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-03 00:25:02 -0500

    added publicpath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -6,9 +6,10 @@ const packageJson = require('../package.json');
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: 'production', // When we set mode to production that's gonna cause webpack to run slightly differently. It's gonna make sure that all the JavaScript files that are built get somewhat optimized. Takes a little bit longer to run webpack in production mode. However, it's gonna make sure that we get a much more production-specific build coming out.
+  mode: 'production',
   output: {
-    filename: '[name].[contenthash].js', // So this ensures that whenever we build some files for production, all the different files that are built are gonna use this as a template to figure out how to name them. We're gonna first put down the name of the file that was created, and then a hash of the contents of the file. This is done primarily for caching issues.
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
