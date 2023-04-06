e8fc141828fb7c6559966ab3861f6ee3b1bacdc5
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-06 18:34:14 -0500

    using browser history in isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createMemoryHistory } from 'history'; // React router internally use this history library for get access to history related material.
+import { createMemoryHistory, createBrowserHistory } from 'history'; // React router internally use this history library for get access to history related material.
 
 // Mount function to start up the app
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory(); // if we were given a default history, let's use it. I'm going to assign that to history. Otherwise, if we did not get one, we will default to creating our own memory history.
   onNavigate && history.listen(onNavigate); // Then, every time a user clicks on the link, we're going to update our memory history and the memory history will automatically call the callback.
   ReactDOM.render(<App history={history} />, el);
 
@@ -23,7 +23,8 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot, {}); // {} because onNavigate not exits in isolation running.
+    mount(devRoot, { defaultHistory: createBrowserHistory() }); // {} because onNavigate not exits in isolation running.
+    // So in short, whenever we call the mount function when we are in isolation, we're going to create a browser history instance and provide it on the options object as a property called default history. We are only providing a default history if we are in development.
   }
 }
 
